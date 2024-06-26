c898e91f8ae46ba5d36d8ecd620a2952ee548633
Dilnavoz-tech <135261234+Dilnavoz-tech@users.noreply.github.com>
2024-06-26 19:52:17 +0500

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "dilnavoz_moose",
+        "USER": "doadmin",
+        "PASSWORD": "AVNS_feJOFOu7KkwbdfMc3JX",
+        "HOST": "do-db-do-user-16947834-0.c.db.ondigitalocean.com",
+        "PORT": "25060",
     }
 }
 
